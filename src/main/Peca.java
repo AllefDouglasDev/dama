404972fa1490797cbcf44d3bbc60088929b1d622
Allef Douglas <allef.douglas@aftisolucoes.com.br>
2019-11-01 16:26:50 -0300

    Removendo waring de serialVersionUID

--- src/main/Peca.java
+++ src/main/Peca.java
@@ -8,6 +8,8 @@ import javax.swing.JButton;
 
 public class Peca extends JButton
 {
+	private static final long serialVersionUID = 1L;
+
 	private static int tamanho = 64;
 	
 	private Estado estado;
