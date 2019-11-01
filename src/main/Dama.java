99621297eeb44a6de2011ff5ffdb287117592271
Allef Douglas <allef.douglas@aftisolucoes.com.br>
2019-11-01 16:18:19 -0300

    Capturando pra baixo com preta e pra cima com branca

--- src/main/Dama.java
+++ src/main/Dama.java
@@ -6,7 +6,7 @@ public class Dama {
 	
 	private ArrayList<Casa> casasPermitidas;
 	
-	public Estado[][] posicoesIniciais() {
+	public Estado[][] getPosicoesIniciais() {
 		Estado[][] posicoes = new Estado[8][8];
 		
 		limparPosicoes(posicoes);
--- src/main/Tabuleiro.java
+++ src/main/Tabuleiro.java
@@ -8,42 +8,44 @@ import javax.swing.JPanel;
 
 public class Tabuleiro extends JPanel
 {
+	private static final long serialVersionUID = 1L;
 	private Dama dama;
 	private PecaMelhor[][] tabuleiro;
-	private int jogadorAtual;
-	private int currentX = -1;
-	private int currentY = -1;
+	private int jogadorAtual = 1;
+	private PecaMelhor pecaAMover;
 
 	public Tabuleiro()
 	{
 		dama = new Dama();
-		jogadorAtual = 1;
 		
 		setLayout(new GridLayout(8,8));
 		tabuleiro = new PecaMelhor[8][8];
 
-		for(int l=0;l<8;l++)
-			for(int c=0;c<8;c++)
+		for(int l = 0; l < 8; l++)
+			for(int c = 0; c < 8; c++)
 			{
-				tabuleiro[c][l] = new PecaMelhor();
+				PecaMelhor peca = new PecaMelhor();
+				peca.setCasa(new Casa(c, l));
+				peca.addActionListener(onPecaClicada(peca, c, l));
+				
+				tabuleiro[c][l] = peca;
+				
 				add(tabuleiro[c][l]);
-				tabuleiro[c][l].addActionListener(onPecaClicada(c, l));
 			}
 
-		
-		Estado[][] posicoesIniciais = dama.posicoesIniciais();
-		
-		for (int i = 0; i < 8; i++) {
-			for (int j = 0; j < 8; j++) {
-				tabuleiro[j][i].setEstado(posicoesIniciais[j][i]);
-			}
-		}
+		preencherTabuleiro();
 	}
 	
-	public ActionListener onPecaClicada(final int x, final int y) {
+	public void preencherTabuleiro() {
+		Estado[][] posicoesIniciais = dama.getPosicoesIniciais();
+		
+		for (int i = 0; i < 8; i++)
+			for (int j = 0; j < 8; j++)
+				tabuleiro[j][i].setEstado(posicoesIniciais[j][i]);
+	}
+	
+	public ActionListener onPecaClicada(final PecaMelhor pecaClicada, final int x, final int y) {
 		return e -> {
-			PecaMelhor pecaClicada = tabuleiro[x][y];
-			
 			switch (pecaClicada.getEstado()) {
 				case VAZIO:
 					pecaVaziaClicada(pecaClicada, x, y);
@@ -55,45 +57,42 @@ public class Tabuleiro extends JPanel
 					pecaPretaClicada(pecaClicada, x, y);
 					break;
 			}
-			// Define a ultima peca clicada
-			currentX = x;
-			currentY = y;
+			
 		};
 	}
 
 	public void pecaVaziaClicada(PecaMelhor pecaClicada, int x, int y) {
-		if (currentX < 0) return;
+		if (pecaAMover == null) return;
 		
 		Casa casaAMover = new Casa(x, y);
-		PecaMelhor pecaAMover = tabuleiro[currentX][currentY];
-		pecaAMover.setCasa(new Casa(currentX, currentY));
 		
 		if ((jogadorAtual == 1 && pecaAMover.getEstado() != Estado.BRANCO) ||
 			(jogadorAtual == 2 && pecaAMover.getEstado() != Estado.PRETO))
 			return;
 		
 		if (dama.eCasaPermitida(casaAMover) && pecaAMover.getCasasProximas().contains(casaAMover)) {
-			tabuleiro[x][y].setEstado(pecaAMover.getEstado());
-			tabuleiro[currentX][currentY].setEstado(Estado.VAZIO);
+			mover(pecaAMover, x, y);
 			jogadorAtual = jogadorAtual == 1 ? 2 : 1;
 			return;
 		} 
 		
-		boolean moveu = moverParaCasaDistante(casaAMover, pecaAMover);
-		
-		if (moveu) {
+		if (moverParaCasaDistante(casaAMover, pecaAMover)) {
 			jogadorAtual = jogadorAtual == 1 ? 2 : 1;
 		}
 		
 		calcularPecas();
 	}
 	
+	
 	public void pecaBrancaClicada(PecaMelhor pecaClicada, int x, int y) {
-		// System.out.println("x " + x + " y " + y);
+		pecaAMover = pecaClicada;
+		pecaAMover.setCasa(new Casa(x, y));
 	}
 	
+	
 	public void pecaPretaClicada(PecaMelhor pecaClicada, int x, int y) {
-		
+		pecaAMover = pecaClicada;
+		pecaAMover.setCasa(new Casa(x, y));
 	}
 	
 	public boolean moverParaCasaDistante(Casa casaAMover, PecaMelhor pecaAMover) {
@@ -101,39 +100,84 @@ public class Tabuleiro extends JPanel
 		ArrayList<Casa> casasDistantes = pecaAMover.getCasasDistantes();
 		ArrayList<Casa> casasProximas = pecaAMover.getCasasProximas();
 		
-		if (casasDistantes.contains(casaAMover)) {
-			if (pecaAMover.getEstado() == Estado.BRANCO) {
-				Casa jumpUpLeft = casasDistantes.get(0);
-				Casa jumpUpRight = casasDistantes.get(1);
-				
-				if (casaAMover.equals(jumpUpRight)) {
-					Casa upRight = casasProximas.get(1);
-					PecaMelhor pecaASerComida = tabuleiro[upRight.getX()][upRight.getY()];
-					if (pecaASerComida.getEstado() == Estado.PRETO) {
-						tabuleiro[upRight.getX()][upRight.getY()].setEstado(Estado.VAZIO);
-						tabuleiro[casaAMover.getX()][casaAMover.getY()].setEstado(pecaAMover.getEstado());
-						tabuleiro[currentX][currentY].setEstado(Estado.VAZIO);
-						moveu = true;
-					}
-				} else if (casaAMover.equals(jumpUpLeft)) {
-					Casa upLeft = casasProximas.get(0);
-					PecaMelhor pecaASerComida = tabuleiro[upLeft.getX()][upLeft.getY()];
-					if (pecaASerComida.getEstado() == Estado.PRETO) {
-						tabuleiro[upLeft.getX()][upLeft.getY()].setEstado(Estado.VAZIO);
-						tabuleiro[casaAMover.getX()][casaAMover.getY()].setEstado(pecaAMover.getEstado());
-						tabuleiro[currentX][currentY].setEstado(Estado.VAZIO);
-						moveu = true;
-					}
-				}
-			} else {
-				
+		if (!casasDistantes.contains(casaAMover)) return false;
+		
+		if (pecaAMover.getEstado() == Estado.BRANCO) 
+			moveu = comerComPecaBranca(casaAMover, pecaAMover, casasDistantes, casasProximas); 
+		else
+			moveu = comerComPecaPreta(casaAMover, pecaAMover, casasDistantes, casasProximas); 
+		return moveu;
+	}
+
+	private boolean comerComPecaBranca(Casa casaAMover, PecaMelhor pecaAMover,
+			ArrayList<Casa> casasDistantes, ArrayList<Casa> casasProximas) {
+		boolean moveu = false;
+		
+		Casa jumpUpLeft = casasDistantes.get(0);
+		Casa jumpUpRight = casasDistantes.get(1);
+		
+		if (casaAMover.equals(jumpUpRight)) {
+			Casa upRight = casasProximas.get(1);
+			PecaMelhor pecaASerComida = tabuleiro[upRight.getX()][upRight.getY()];
+			
+			if (pecaASerComida.getEstado() == Estado.PRETO) {
+				tabuleiro[upRight.getX()][upRight.getY()].setEstado(Estado.VAZIO);
+				mover(pecaAMover, casaAMover.getX(), casaAMover.getY());
+				moveu = true;
+			}
+		} else if (casaAMover.equals(jumpUpLeft)) {
+			Casa upLeft = casasProximas.get(0);
+			PecaMelhor pecaASerComida = tabuleiro[upLeft.getX()][upLeft.getY()];
+			
+			if (pecaASerComida.getEstado() == Estado.PRETO) {
+				tabuleiro[upLeft.getX()][upLeft.getY()].setEstado(Estado.VAZIO);
+				mover(pecaAMover, casaAMover.getX(), casaAMover.getY());
+				moveu = true;
 			}
 		}
 		
 		return moveu;
 	}
 	
-	public void calcularPecas() {
+	private boolean comerComPecaPreta(Casa casaAMover, PecaMelhor pecaAMover,
+			ArrayList<Casa> casasDistantes, ArrayList<Casa> casasProximas) {
+		boolean moveu = false;
+		
+		Casa jumpDownLeft = casasDistantes.get(2);
+		Casa jumpDownRight = casasDistantes.get(3);
+		
+		if (casaAMover.equals(jumpDownRight)) {
+			Casa downRight = casasProximas.get(3);
+			PecaMelhor pecaASerComida = tabuleiro[downRight.getX()][downRight.getY()];
+			
+			if (pecaASerComida.getEstado() == Estado.BRANCO) {
+				tabuleiro[downRight.getX()][downRight.getY()].setEstado(Estado.VAZIO);
+				mover(pecaAMover, casaAMover.getX(), casaAMover.getY());
+				moveu = true;
+			}
+		} else if (casaAMover.equals(jumpDownLeft)) {
+			Casa downLeft = casasProximas.get(2);
+			PecaMelhor pecaASerComida = tabuleiro[downLeft.getX()][downLeft.getY()];
+			
+			if (pecaASerComida.getEstado() == Estado.BRANCO) {
+				tabuleiro[downLeft.getX()][downLeft.getY()].setEstado(Estado.VAZIO);
+				mover(pecaAMover, casaAMover.getX(), casaAMover.getY());
+				moveu = true;
+			}
+		}
+		
+		return moveu;
+	}
+	
+	public void mover(PecaMelhor peca, int paraX, int paraY) {
+		tabuleiro[paraX][paraY].setCasa(peca.getCasa());
+		tabuleiro[paraX][paraY].setEstado(peca.getEstado());
+		
+		tabuleiro[peca.getCasa().getX()][peca.getCasa().getY()].setEstado(Estado.VAZIO);
+	}
+	
+	
+	public int[] calcularPecas() {
 		int pretas = 0;
 		int brancas = 0;
 		for (int i = 0; i < 8; i++) { 
@@ -145,7 +189,6 @@ public class Tabuleiro extends JPanel
 			}
 		}
 		
-		System.out.println("Pretas: " + pretas);
-		System.out.println("Brancas: " + brancas);
+		return new int[]{ pretas, brancas };
 	}
 }
